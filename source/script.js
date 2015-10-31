8f18a8bc03df13ba61fe04ff9145b2fd2373b8cf
Laurens Martos <l-martos@hotmail.com>
2015-10-31 20:51:21 +0100

    added ability to login

--- source/script.js
+++ source/script.js
@@ -1,4 +1,52 @@
 $("#registerNavButton").click(function(){
 		window.location.replace("./register.html");
 	});
+$("#loginNavButton").click(function(){
+	login();
+});
+
+
+
+if(localStorage.getItem("token") != null){
+  	console.log("nickname: " + localStorage.getItem("nickname"));
+	console.log("token: " + localStorage.getItem("token"));
+  	updateNavBar();
+  };
+
+
+function login() {
+	var nickname = document.getElementById("nicknameNavBarField").value;
+	var password = document.getElementById("passwordNavBarField").value;
+
+	$.ajax({
+		type: "POST",
+		contentType: "application/json",
+		url: "http://localhost:8080/RestServer/api/users/login",
+		data: "{\"nickname\": \""+nickname+"\", \"password\": \""+password+"\"}",
+		
+	}).fail(function(jqXHR,	textStatus, errorThrown) {	
+  		console.log("API Request failed: " + errorThrown);	
+	}).done(function(data) {  	
+		localStorage.setItem("token", data.entity.value);
+		localStorage.setItem("nickname", nickname);
+		console.log("nickname: " + localStorage.getItem("nickname"));
+		console.log("token: " + localStorage.getItem("token"));
+		updateNavBar();
+
+   	});
+}
+
+function updateNavBar(){
+		var navBarRight = document.getElementById("navBarRight");
+		$(navBarRight).empty();
+		$(navBarRight).append(localStorage.getItem("nickname")+" ");
+		$(navBarRight).append("<button type=\"button\" class=\"btn btn-default\" id=\"logoutNavButton\">Logout</button>");
+		$("#logoutNavButton").click(function(){
+			localStorage.removeItem("token");
+			location.reload();
+
+		});
+
+}
+
 
